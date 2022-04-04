faa2385ae662f7ea03d63bf3ee9993b60ccf7264
WONCEF <77626000+Moncef-dd@users.noreply.github.com>
2022-04-04 23:33:39 +0100

    Create conv.py

---
+++ conv.py
@@ -0,0 +1,18 @@
+from gtts import gTTS
+
+# If you don't have gtts go to the terminal and write pip install gtts
+import os
+
+# Reading the files
+f = open("test.txt", "r")
+x = f.read()
+
+# Specifying language:
+language = 'en'  # You can specify any language. Reffer to the gtts documentation
+# simply write this down for better sound
+audio = gTTS(text=x, lang=language, slow=False)
+
+# Now save the audio file
+# You can save by any name you wish
+audio.save("3.wav")
+os.system("3.wav")
